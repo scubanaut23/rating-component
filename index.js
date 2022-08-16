d33771f8cf9dcd2dd3e08b4676f8ecd34a494405
scubanaut23 <scubanaut23@yahoo.de>
2022-08-16 23:16:42 +0200

    fixed button display in JS

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const value = 0;
 const ratings = document.querySelectorAll('.star');
 
-ratings.forEach((element) => {
-  element.addEventListener('click', function handleClick(event) {
-    element.setAttribute(
-      'style',
-      'background-color:  hsl(25, 97%, 53%); color: hsl(0, 0%, 100%);'
-    );
+const ratingClick = (e) => {
+  ratings.forEach((button) => {
+    console.log(e);
+    button.classList.remove('rating-click');
   });
+  if (e.target.classList.contains('star')) {
+    e.target.classList.add('rating-click');
+  } else {
+    e.target.parentElement.classList.add('rating-click');
+  }
+  value = e.target.value;
+};
+
+ratings.forEach((button) => {
+  button.addEventListener('click', ratingClick);
 });
 
 const submit = document.querySelector('.button');
