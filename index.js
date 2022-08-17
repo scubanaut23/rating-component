1c1f400bef3644d78eb3fc53ef2fb139aa4fa824
scubanaut23 <scubanaut23@yahoo.de>
2022-08-17 22:04:38 +0200

    the JS is working

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const value = 0;
 const ratings = document.querySelectorAll('.star');
+const rated = document.querySelector('.class-rate');
+const submit = document.querySelector('.button');
+let value = 3;
+const cardRating = document.querySelector('.cardRate');
+const card_1 = document.querySelector('.card');
+const card_2 = document.querySelector('.card-content2');
+submit.addEventListener('click', onSubmit);
+
+function onSubmit() {
+  card_1.classList.add('hide');
+  cardRating.textContent = value;
+  card_2.classList.remove('hide');
+}
 
 const ratingClick = (e) => {
   ratings.forEach((button) => {
-    console.log(e);
     button.classList.remove('rating-click');
   });
   if (e.target.classList.contains('star')) {
@@ -11,15 +22,10 @@ const ratingClick = (e) => {
   } else {
     e.target.parentElement.classList.add('rating-click');
   }
-  value = e.target.value;
+  value = e.target.textContent;
+  console.log(value);
 };
 
 ratings.forEach((button) => {
   button.addEventListener('click', ratingClick);
-});
-
-const submit = document.querySelector('.button');
-
-submit.addEventListener('click', function handleClick(event) {
-  location.href = './thankyou.html';
 });
